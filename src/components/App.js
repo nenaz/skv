b3eedd387f2fd98a25c4395d1ee494006f3277d3
nenaz <mr444@mail.ru>
2017-10-11 23:51:39 +0400

    FIX

--- src/components/App.js
+++ src/components/App.js
@@ -16,17 +16,18 @@ class App extends Component {
       error: null,
       rates: {}
     };
+    this.startGetRatesFromServer = this.startGetRatesFromServer.bind(this)
     this.handleClickGetRate = this.handleClickGetRate.bind(this)
+    this.getAccountsFromServer = this.getAccountsFromServer.bind(this)
   }
 
-  handleClickGetRate(str) {
+  handleClickGetRate(str,e,t,y) {
     console.log('handleClickGetRate')
     var testClientId = "2041234";
     var arr = [];
     var obj = {
         client: testClientId,
         sym: 'USD/RUB',
-        // s: 10000,
         s: 10000,
         cur: 'RUB'
     };
@@ -44,13 +45,32 @@ class App extends Component {
     }, 5000)
   }
 
-  componentDidMount() {
-    console.log('componentDidMount')
-    this.ws = new WebSocket('ws://10.255.14.131:8099', 'echo-protocol')
-    this.ws.onmessage = e => this.setState({ rates: Object.values(JSON.parse(e.data)) })
+  getAccountsFromServer(e) {
+    // this.setState({
+    //   accounts: this.handleClickGetRate('GetAccounts')
+    // })
+    console.log(e)
+  }
+
+  wsOnMessageEvent(e) {
+    console.log(e)
+  }
+
+  componentWillMount() {
+    console.log('componentWillMount')
+    this.ws = new WebSocket('ws://192.168.0.104:8099', 'echo-protocol')
+    // this.ws.onmessage = e => this.setState({ rates: Object.values(JSON.parse(e.data)) })
+    this.ws.onmessage = e => this.wsOnMessageEvent(JSON.parse(e.data))
     this.ws.onerror = e => this.setState({ error: 'WebSocket error' })
     this.ws.onclose = e => !e.wasClean && this.setState({ error: `WebSocket error: ${e.code} ${e.reason}` })
     this.startGetRatesFromServer();
+    this.getAccountsFromServer();
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount')
+    // this.ws = new WebSocket('ws://10.255.14.131:8099', 'echo-protocol')
+    
   }
 
   componentWillUnmount() {
