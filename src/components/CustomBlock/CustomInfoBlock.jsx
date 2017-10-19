10b4dca00c0689531d636de5c91222f45cadfabb
Aleksey Gribov <gribov_an@open.ru>
2017-10-19 16:09:49 +0300

    FIX

--- src/components/CustomBlock/CustomInfoBlock.jsx
+++ src/components/CustomBlock/CustomInfoBlock.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import styles from './css/CustomInfoBlock.css'
 import {connect} from 'react-redux'
+import {strValues} from '../../js/consts'
 
 const CustomInfoBlock = (props) => {
     const rateObj = (props.rates).filter(item => item.sym === 'USD/RUB')
     const rate = (true) ? rateObj[0].a : rateObj[0].b
-    const summ = props.inputValue * rate
+    const summ = (props.selectedButton) ? props.inputValue * rate : props.inputValue / rate
     console.log(rateObj)
     return (
         <div className={styles.skvDescText}>
-            <div className="SkvDescriptionLin1">На счёт будет зачислено {summ}</div>
+            <div className="SkvDescriptionLin1">{(props.selectedButton) ? strValues.fromAccountText : strValues.toAccountText} {summ}</div>
             <div className="SkvDescriptionLin2"> по курсу  {rate}  за 1  <span className="ccA2F USD"></span>*</div>
         </div>
     )
--- src/js/consts.js
+++ src/js/consts.js
@@ -2,3 +2,7 @@ export const INCREMENT = 'INCREMENT'
 export const CHANGEVALUE = 'CHANGEVALUE'
 export const SELECTTRANSACTIONVALUE = 'SELECTTRANSACTIONVALUE'
 export const CHANGERATES = 'CHANGERATES'
+export const strValues = {
+    toAccountText: 'На счет будет зачислено',
+    fromAccountText: 'Со счета будет списано'
+}
