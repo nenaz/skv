a6b4e658f3b53a92694b8f0c9faaee9ea79e023b
nenaz <mr444@mail.ru>
2017-11-05 14:29:17 +0400

    addons

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,14 +1,37 @@
 import React, { Component } from 'react';
-import {Provider} from 'react-redux'
-import App from './App'
-import store from '../store'
+// import {Provider} from 'react-redux'
+// import App from './App'
+// import store from '../store'
+import styles from '../css/Root.css'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
 class Root extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {}
+    }
+
     render() {
-        return (
-            <Provider store={store}>
-                <App />
-            </Provider>
+        return (<ReactCSSTransitionGroup
+            transitionName={{
+                enter: styles['enter'],
+                enterActive: styles['enter-active'],
+                leave:  styles['leave'],
+                leaveActive:  styles['leave-active'],
+                appear:  styles['appear'],
+                appearActive:  styles['appear-active']
+            }}
+            // transitionName='expamle'
+            transitionAppear={true}
+            transitionEnter={true}
+            // transitionLeave={false}
+            transitionAppearTimeout={1000}
+            transitionEnterTimeout={1500}
+            transitionLeaveTimeout={2500}
+        >
+            <h1 className={styles['h1cl']}>Fading at Initial Mount</h1>
+
+        </ReactCSSTransitionGroup>
         )
     }
 }
